139256835f191efe2ef896e3b2bd34eb94096964
deanone <athanasios.salamanis@gmail.com>
2022-03-02 12:36:07 +0200

    Fixed indentation (4 spaces).

--- quicksort_lomuto.c
+++ quicksort_lomuto.c
@@ -4,35 +4,35 @@
 
 void swap(int *A, int i, int j)
 {
-	int temp = A[i];
-	A[i] = A[j];
-	A[j] = temp;
+    int temp = A[i];
+    A[i] = A[j];
+    A[j] = temp;
 }
 
 int partition(int *A, int lo, int hi)
 {
-	int pivot = A[hi];
-	int i = lo;
-	for (int j = lo; j < hi; ++j)
-	{
-		if (A[j] < pivot)
-		{
-			swap(A, i, j);
-			i++;
-		}
-	}
-	swap(A, i, hi);
-	return i;
+    int pivot = A[hi];
+    int i = lo;
+    for (int j = lo; j < hi; ++j)
+    {
+        if (A[j] < pivot)
+        {
+            swap(A, i, j);
+            i++;
+        }
+    }
+    swap(A, i, hi);
+    return i;
 }
 
 void quicksort(int *A, int lo, int hi)
 {
-	if (lo < hi)
-	{
-		int p = partition(A, lo, hi);
-		quicksort(A, lo, p - 1);
-		quicksort(A, p + 1, hi);
-	}
+    if (lo < hi)
+    {
+        int p = partition(A, lo, hi);
+        quicksort(A, lo, p - 1);
+        quicksort(A, p + 1, hi);
+    }
 }
 
 int irand(int low, int high)
@@ -42,59 +42,59 @@ int irand(int low, int high)
 
 int main(int argc, char *argv[]) 
 {
-	if (argc == 4)
-	{
-		// for random integers generation
-		srand(time(NULL));
-	
-		int N = atoi(argv[1]);
-		int low = atoi(argv[2]);
-		int high = atoi(argv[3]);
-		
-		// Allocate memory for the array
-		int *A;
-		A = malloc(N * sizeof(int));
-		
-		// Set values to array and print them before sorting
-		printf("Unsorted array:\n");
-		for (int i = 0; i < N; ++i)
-		{
-			A[i] = irand(low, high);
-			if (i < (N - 1))
-			{
-				printf("%d ", A[i]);
-			}
-			else
-			{
-				printf("%d\n", A[i]);
-			}
-		}
-		
-		// Sort array
-		quicksort(A, 0, N - 1);
-		
-		// Print sorted array
-		printf("Sorted array:\n");
-		for (int i = 0; i < N; ++i)
-		{
-			if (i < (N - 1))
-			{
-				printf("%d ", A[i]);
-			}
-			else
-			{
-				printf("%d\n", A[i]);
-			}
-		}
-		
-		// free memory
-		free(A);
-	}
-	else
-	{
-		printf("Usage:\n");
-		printf("./quicksort_lomuto.out [number of values in array] [lowest of values range] [highest of values range]\n");
-	}
+    if (argc == 4)
+    {
+        // for random integers generation
+        srand(time(NULL));
+    
+        int N = atoi(argv[1]);
+        int low = atoi(argv[2]);
+        int high = atoi(argv[3]);
+        
+        // Allocate memory for the array
+        int *A;
+        A = malloc(N * sizeof(int));
+        
+        // Set values to array and print them before sorting
+        printf("Unsorted array:\n");
+        for (int i = 0; i < N; ++i)
+        {
+            A[i] = irand(low, high);
+            if (i < (N - 1))
+            {
+                printf("%d ", A[i]);
+            }
+            else
+            {
+                printf("%d\n", A[i]);
+            }
+        }
+        
+        // Sort array
+        quicksort(A, 0, N - 1);
+        
+        // Print sorted array
+        printf("Sorted array:\n");
+        for (int i = 0; i < N; ++i)
+        {
+            if (i < (N - 1))
+            {
+                printf("%d ", A[i]);
+            }
+            else
+            {
+                printf("%d\n", A[i]);
+            }
+        }
+        
+        // free memory
+        free(A);
+    }
+    else
+    {
+        printf("Usage:\n");
+        printf("./quicksort_lomuto.out [number of values in array] [lowest of values range] [highest of values range]\n");
+    }
 
-	return 0;
+    return 0;
 }
